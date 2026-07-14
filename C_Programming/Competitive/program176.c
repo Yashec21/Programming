91e960d1571941010661b92eef34e98fe5f0dadb
Yashec21 <yashpatilec21@gmail.com>
2026-07-14 19:36:37 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program176.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+typedef unsigned int UNIT;
+
+
+UNIT OffBit(UNIT iNo)
+{
+    UNIT iAns=0;
+    UNIT iMask =0x40;
+
+    iAns = (iNo &(~iMask));
+   
+    return iAns;
+
+        
+}
+int main()
+{
+    UNIT iNo=0;
+    UNIT bRet =0;
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    bRet = OffBit(iNo);
+
+    printf("%d",bRet);
+}
