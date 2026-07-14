3ab823891ed510be1ce7e1b49c6f911dce188091
Yashec21 <yashpatilec21@gmail.com>
2026-07-14 19:35:15 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program175.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+typedef int BOOL;
+typedef unsigned int UNIT;
+
+# define TRUE 1
+# define FALSE 0
+
+BOOL ChkBit(UNIT iNo)
+{
+    UNIT iAns=0;
+    UNIT iMask =0x80000001;
+
+    iAns = iNo & iMask;
+    if(iAns== iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+
+
+        
+}
+int main()
+{
+    UNIT iNo=0;
+    UNIT bRet =0;
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    bRet = ChkBit(iNo);
+
+    if(bRet == TRUE)
+    {
+        printf("bit is ON");
+    }
+    else
+    {
+        printf("bit is OFF");
+    }
+}
