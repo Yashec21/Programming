e9510b9e9b11bf474555a873f4ebab1bd96a0c31
Yashec21 <yashpatilec21@gmail.com>
2026-07-15 23:27:40 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program179.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+typedef unsigned int UNIT;
+
+
+UNIT ToggalBit(UNIT iNo)
+{
+    UNIT iAns=0;
+    UNIT iMask =0x240;
+
+    iAns = iNo ^ iMask;
+   
+    return iAns;
+        
+}
+int main()
+{
+    UNIT iNo=0;
+    UNIT iRet =0;
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    iRet = ToggalBit(iNo);
+
+    printf("%u",iRet);
+}
