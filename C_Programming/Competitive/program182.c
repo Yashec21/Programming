b746e6f69012ef524e6b588a785d37944c0a76a4
Yashec21 <yashpatilec21@gmail.com>
2026-07-16 22:57:40 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program182.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+typedef unsigned int UNIT;
+
+int OffBit(UNIT iNo,UNIT iPos)
+{
+    UNIT iAns=0;
+    UNIT iMask =0x1;
+
+    iMask = iMask <<(iPos-1);
+    iAns = iNo & (~iMask) ;
+    
+    return iAns;
+    
+
+
+        
+}
+int main()
+{
+    UNIT iNo=0;
+    UNIT iPos=0;
+
+    UNIT iRet =0;
+
+    printf("Enter number \n");
+    scanf("%d",&iNo);
+
+    printf("Enter position \n");
+    scanf("%d",&iPos);
+
+    iRet = OffBit(iNo,iPos);
+
+    printf("%u",iRet);
+}
