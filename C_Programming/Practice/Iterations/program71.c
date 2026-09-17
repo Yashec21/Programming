b7b05e9c6093a44b452f85287185b70a79e9e0d1
Yashec21 <yashpatilec21@gmail.com>
2026-09-17 19:36:59 +0530

    Add files via upload

---
+++ C_Programming/Practice/Iterations/program71.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while ( iCnt <=5)  // joparyant(while) iCnt 5 kivva 5 peksha lahan ahe
+    {
+        printf("Jay Ganesh...\n");
+        iCnt++;
+    }
+
+    return 0;
+    
+}
