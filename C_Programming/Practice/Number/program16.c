bce5352c96b8608614aec0b72b665ac5eda92d87
Yashec21 <yashpatilec21@gmail.com>
2026-09-17 17:36:35 +0530

    Add files via upload

---
+++ C_Programming/Practice/Number/program16.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckEvenOdd(int iNo)
+{
+    if ((iNo % 2) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false; //defallut values becuse is inertnally flase is 0
+    
+    printf("Enter Number to Check whether it is Even or Odd : "); //whether manje ho ki nhi 
+    scanf("%d",&iValue);
+
+    bRet = CheckEvenOdd(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is Even\n",iValue);
+    }
+    else
+    {
+        printf("%d is Odd\n",iValue);
+    }
+    return 0;
+}
